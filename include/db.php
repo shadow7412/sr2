2bc1d50bbf689340db410b8caa84f9e8c2457e49
Shadow <huffy7412@gmail.com>
2013-08-01 22:07:01 +1000

    Added lanpanel credentials

--- include/db.php
+++ include/db.php
@@ -1,5 +1,5 @@
 <?php
-$db = new mysqli("127.0.0.1","root","","lanpanel_shadowreader");
+$db = new mysqli("127.0.0.1","lanpanel_steven",'&M2Ar-Pr*$mF',"lanpanel_shadowreader");
 session_name("rdr");
 session_start();
 $_user = @$db->real_escape_string($_SESSION['user']);
